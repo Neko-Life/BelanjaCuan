141debb00389e57627a7b4b57d87305b24822b9b
Neko-Life <nekolife123579@gmail.com>
2022-10-14 08:10:32 +0700

    More update

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ const products = [
 const USERNAME_INPUT_ID = "username-input";
 const PASSWORD_INPUT_ID = "password-input";
 const LOGIN_BUTTON_ID = "login-button";
-const SIGNUP_BUTTON_ID = "";
+const SIGNUP_BUTTON_ID = "signup-button";
 
 /**
  * @typedef {object} UserData
@@ -170,6 +170,7 @@ const removeUser = (username) => {
 }
 
 let currentUser = null;
+let cart = {};
 
 /**
  * Get logged-in user for this session
@@ -185,15 +186,94 @@ const getCurrentUser = () => {
  * @returns {UserData}
  */
 const setCurrentUser = (user) => {
-    return currentUser = user;
+    currentUser = user;
+
+    const element = document.getElementById("login-nav-button");
+    if (element) element.innerText = user?.username || "Login";
+
+    return currentUser;
+}
+
+/**
+ * 
+ * @param {URL} url 
+ * @returns 
+ */
+const createUrlParam = (url) => {
+    if (Object.keys(cart).length) url.searchParams.set("cart", JSON.stringify(cart));
+
+    const users = [];
+    for (const [key, user] of _users) {
+        users.push(user);
+    }
+
+    console.log(users);
+    if (users.length) {
+        url.searchParams.set("users", JSON.stringify(users));
+    }
+
+    const user = getCurrentUser();
+    console.log(user);
+    if (user) url.searchParams.set("user", user.username);
+
+    return url;
+}
+
+const parseUrlParam = () => {
+    const searchData = new URLSearchParams(window.location.href);
+    const keys = [...searchData.keys()];
+    const values = [...searchData.values()];
+
+    console.log(values);
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        const value = values[i];
+        if (key.endsWith("cart")) {
+            const cartJson = value;
+            if (cartJson) cart = JSON.parse(cartJson);
+        }
+
+        if (key.endsWith("users")) {
+            const users = value;
+            if (users) {
+                const parse = JSON.parse(users);
+                for (const data of parse) {
+                    if (!data) continue;
+                    setUser(data.username, data);
+                }
+            }
+        }
+
+        if (key.endsWith("user")) {
+            const user = value;
+            if (user) {
+                const userData = getUser(user);
+                if (userData) {
+                    setCurrentUser(userData);
+                }
+            }
+        }
+    }
+}
+
+const validNamePassword = () => {
+    const elementUsername = document.getElementById(USERNAME_INPUT_ID);
+    const elementPassword = document.getElementById(PASSWORD_INPUT_ID);
+    if (!elementUsername.value?.match(/[a-zA-Z0-9 _-]+/)
+        || !elementPassword.value?.match(/[a-zA-Z0-9 _-]+/)) {
+        return false;
+    }
+    return true;
 }
 
 const goToPage = (path) => {
-    if (path !== "/" && !getCurrentUser()) {
-        alert("You're not logged in! Please log in first!");
+    if (path.startsWith("/invoice.html") && !getCurrentUser()) {
+        alert("You're not logged in! Please login first!");
         return;
     }
-    window.location.pathname = path;
+    const url = new URL(window.location.origin + path);
+
+    window.location.href = createUrlParam(url);
 }
 
 const redBorder1px = (element) => {
@@ -231,23 +311,30 @@ const createProductCard = ({ nama, harga, namagambar, stock } = {}) => {
         }
     }
 
+    if (cart[nama]?.qty) {
+        stock -= cart[nama].qty;
+    }
+
     return `<div class="product-cell">
         <img class="product-img" src="gambarBaju/${namagambar}" />
         <div class="product-name">${nama}</div>
         <div class="product-price">Rp. ${hargaStr}</div>
-        <div class="product-desc">Stok: ${stock}</div>
+        <div class="product-desc" id="product-desc-${nama}">Stok: ${stock}</div>
         <button class="product-add" id="product-add-${nama}">+ Tambah ke keranjang</button>
-        <a href="invoice.html"><button class="product-buy" >Beli</button></a>
+        <button class="product-buy" >Beli</button>
     </div>`;
-
 }
 
 const login = () => {
+    if (!validNamePassword()) {
+        return;
+    }
+
     const elementUsername = document.getElementById(USERNAME_INPUT_ID); // Id element input username
     const elementPassword = document.getElementById(PASSWORD_INPUT_ID); // Id element input password
 
-    const username = elementUsername.attributes.value;
-    const password = elementPassword.attributes.value;
+    const username = elementUsername.value;
+    const password = elementPassword.value;
 
     const userData = getUser(username);
 
@@ -260,16 +347,20 @@ const login = () => {
         alert("Invalid username or password");
     } else {
         setCurrentUser(userData);
-        goToPage("page/beranda.html");
+        goToPage("/index.html");
     }
 }
 
 const signUp = () => {
+    if (!validNamePassword()) {
+        return;
+    }
+
     const elementUsername = document.getElementById(USERNAME_INPUT_ID); // Id element input username
     const elementPassword = document.getElementById(PASSWORD_INPUT_ID); // Id element input password
 
-    const username = elementUsername.attributes.value;
-    const password = elementPassword.attributes.value;
+    const username = elementUsername.value;
+    const password = elementPassword.value;
 
     const userData = getUser(username);
 
@@ -288,7 +379,7 @@ const signUp = () => {
         setUser(username, newUser);
         setCurrentUser(newUser);
         alert("Thank you for signing up <3");
-        goToPage("page/beranda.html");
+        goToPage("/index.html");
     }
 }
 
@@ -297,49 +388,89 @@ const signUp = () => {
  */
 const logOut = () => {
     if (confirm("Are you sure you want to log out?")) {
-        setUser(null);
+        setCurrentUser(null);
         goToPage("/");
     }
 }
 
-const resetTable = () => {
-    const elementTable = document.getElementsByClassName("product-table")[0];
-    elementTable.innerHTML = "";
+const setInvoiceUserCreds = (element) => {
+    const user = getCurrentUser();
+    element.innerHTML = `
+    Nama                      : ${user.username}
+    Password                  : ${user.password}
+    `
 }
 
-const showProducts = () => {
+const showProducts = (productsToShow) => {
     const elementTable = document.getElementsByClassName("product-table")[0];
 
-    for (let i = 0; i < products.length; i += 3) {
-        const tr = document.createElement("tr");
+    if (elementTable) {
+        elementTable.innerHTML = "";
+        for (let i = 0; i < productsToShow.length; i += 3) {
+            const tr = document.createElement("tr");
 
-        for (let j = i; j < (i + 3); j++) {
-            if (j === products.length) {
-                break;
+            for (let j = i; j < (i + 3); j++) {
+                if (j === productsToShow.length) {
+                    break;
+                }
+
+                const data = productsToShow[j];
+
+                const td = document.createElement("td");
+                td.innerHTML += createProductCard(data);
+
+                tr.appendChild(td);
             }
 
-            const data = products[j];
-
-            const td = document.createElement("td");
-            td.innerHTML += createProductCard(data);
-
-            tr.appendChild(td);
+            elementTable.appendChild(tr);
         }
-
-        elementTable.appendChild(tr);
     }
 }
 
-let cart = [];
-const loadProductsListener = () => {
-    for (const data of products) {
-        document.getElementById(`product-add-${data.nama}`).addEventListener("click", (...args) => {
-            console.log(args);
-            cart.push(data);
+const loadProductsListener = (productsToListen) => {
+    for (const data of productsToListen) {
+        const element = document.getElementById(`product-add-${data.nama}`);
+        if (element) element.addEventListener("click", (event) => {
+            if (!cart[data.nama]) {
+                cart[data.nama] = { ...data, qty: 0 };
+            }
+            cart[data.nama].qty++;
+            const sisaStock = data.stock - cart[data.nama].qty;
+            if (sisaStock < 0) {
+                alert("Out of stock!");
+                cart[data.nama].qty--;
+                return;
+            }
+            const description = document.getElementById(`product-desc-${data.nama}`);
+            if (description) description.innerText = `Stok: ${sisaStock}`;
         });
     }
 }
 
+const loadInvoiceButtonListeners = () => {
+    const buyButtons = document.getElementsByClassName("product-buy");
+    for (const button of buyButtons) {
+        button.addEventListener("click", () => {
+            goToPage("/invoice.html");
+        });
+    }
+}
+
+const search = () => {
+    const elementSearch = document.getElementById("text-input-search-products");
+    if (elementSearch.value) {
+        const filter = products.filter(product => product.nama.toLowerCase().includes(elementSearch.value.toLowerCase()));
+
+        showProducts(filter);
+        loadProductsListener(filter);
+    } else {
+        showProducts(products);
+        loadProductsListener(products);
+    }
+    loadInvoiceButtonListeners();
+}
+
+parseUrlParam();
 
 // button login onclick
 const elementLoginButton = document.getElementById(LOGIN_BUTTON_ID);
@@ -361,7 +492,39 @@ if (elementPassword) elementPassword.addEventListener("change", () => {
     revertRedBorder1px(elementPassword);
 });
 
-if (window.location.pathname === "/") {
-    showProducts();
-    loadProductsListener();
+const toLoginButton = document.getElementById("login-nav-button");
+if (toLoginButton) toLoginButton.addEventListener("click", () => {
+    if (getCurrentUser()) {
+        logOut();
+        return;
+    }
+
+    goToPage("/login.html");
+});
+
+const homeNavButton = document.getElementById("home-nav-button");
+if (homeNavButton) homeNavButton.addEventListener("click", () => {
+    goToPage("/");
+});
+
+const berandaNavButton = document.getElementById("beranda-nav-button");
+if (berandaNavButton) berandaNavButton.addEventListener("click", () => {
+    goToPage("/index.html");
+});
+
+const invoiceNavButton = document.getElementById("invoice-nav-button");
+if (invoiceNavButton) invoiceNavButton.addEventListener("click", () => {
+    goToPage("/invoice.html");
+});
+
+if (window.location.pathname.startsWith("/")) {
+    showProducts(products);
+    loadProductsListener(products);
+    loadInvoiceButtonListeners();
 }
+
+const buttonSearch = document.getElementById("button-search-products");
+if (buttonSearch) buttonSearch.addEventListener("click", search);
+
+const invoiceUserCreds = document.getElementById("user-creds");
+if (invoiceUserCreds) setInvoiceUserCreds(invoiceUserCreds);
